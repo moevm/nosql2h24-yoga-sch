0ec97dfd41d69d2518f92365405f45d8953929af
elizaveta-andreeva <elizavetaa40@gmail.com>
2024-11-11 12:45:49 +0300

    fix: fix created and updated date in init script

--- mongodb/init-mongo.js
+++ mongodb/init-mongo.js
@@ -8,13 +8,13 @@ db.createCollection('classes', { capped: false });
 db.clients.insertOne(
     {
         "name": "Elizaveta Andreeva",
-        "phone": "+7(999)99-9999",
+        "phone": "+7 (999) 99-9999",
         "gender": "FEMALE",
         "birth_date": "2001-10-28T23:58:18Z",
-        "created_at": "2040-10-28T23:58:18Z",
-        "updated_at": "2040-10-29T23:58:18Z",
+        "created_at": "2024-10-28T23:58:18Z",
+        "updated_at": "2024-10-29T23:58:18Z",
         "password": "11111111",
-        "picture_uri": "https://cdn.example.com",
+        "picture_uri": "cdn.example.com",
         "classes": []
     }
 );
