70ecfea00d2aa0407df64e6e92e0531277899802
purposelessness <purposelessness@ya.ru>
2024-12-06 22:05:44 +0300

    feat(server): add import/export endpoints

---
+++ server/internal/db/admin_panel.go
@@ -0,0 +1,61 @@
+package db
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+func (r MongoRepository) DropDB(ctx context.Context) error {
+	return r.Db().Drop(ctx)
+}
+
+func (r MongoRepository) ImportDB(
+	ctx context.Context,
+	data map[string][]bson.M,
+) error {
+	if err := r.Db().Drop(ctx); err != nil {
+		return err
+	}
+
+	for colName, docs := range data {
+		col := r.Db().Collection(colName)
+		if _, err := col.InsertMany(ctx, docs); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (r MongoRepository) ExportDB(
+	ctx context.Context) (map[string][]bson.M, error) {
+	db := r.Db()
+
+	colNames, err := db.ListCollectionNames(ctx, bson.D{})
+	if err != nil {
+		return nil, err
+	}
+
+	data := make(map[string][]bson.M, len(colNames))
+	for _, colName := range colNames {
+		col := db.Collection(colName)
+
+		cursor, err := col.Find(ctx, bson.D{})
+		if err != nil {
+			return nil, err
+		}
+
+		var docs []bson.M
+		if err = cursor.All(ctx, &docs); err != nil {
+			return nil, err
+		}
+
+		if err := cursor.Close(ctx); err != nil {
+			return nil, err
+		}
+
+		data[colName] = docs
+	}
+
+	return data, nil
+}
--- server/internal/handlers/v1/admin_panel.go
+++ server/internal/handlers/v1/admin_panel.go
@@ -2,7 +2,11 @@ package v1
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
 
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -21,7 +25,40 @@ func (p *AdminPanel) DropDB(
 	ctx context.Context, _ *emptypb.Empty,
 ) (*emptypb.Empty, error) {
 	if err := p.Repo.DropDB(ctx); err != nil {
-		return nil, status.Errorf(codes.Internal, "Internal error: %v", err)
+		return nil, status.Errorf(codes.Internal, "Internal error: %w", err)
 	}
 	return &emptypb.Empty{}, nil
+}
+
+func (p *AdminPanel) ImportDB(
+	ctx context.Context, req *gen.DBData,
+) (*emptypb.Empty, error) {
+	slog.Info(req.Data)
+
+	var data map[string][]bson.M
+	if err := json.Unmarshal([]byte(req.Data), &data); err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintf("Invalid JSON data: %s", err))
+	}
+
+	if err := p.Repo.ImportDB(ctx, data); err != nil {
+		return nil, status.Errorf(codes.Internal, "Internal error: %w", err)
+	}
+
+	return &emptypb.Empty{}, nil
+}
+
+func (p *AdminPanel) ExportDB(
+	ctx context.Context, _ *emptypb.Empty,
+) (*gen.DBData, error) {
+	data, err := p.Repo.ExportDB(ctx)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Internal error: %w", err)
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Internal error: %w", err)
+	}
+
+	return &gen.DBData{Data: string(jsonData)}, nil
 }
