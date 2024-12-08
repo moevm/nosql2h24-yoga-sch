02999065b31450f4ca5de44baf0a3b6dcee796bb
purposelessness <purposelessness@ya.ru>
2024-12-08 16:12:39 +0300

    feat(server): add import db sample

--- server/internal/db/init.go
+++ server/internal/db/init.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
@@ -29,4 +30,53 @@ func New() *mongo.Client {
 	fmt.Println("Connected to MongoDB!")
 
 	return client
+}
+
+func objectIDFromHex(val string) bson.ObjectID {
+	id, err := bson.ObjectIDFromHex(val)
+	if err != nil {
+		panic(fmt.Sprintf("ObjectIDFromHex: %v", err))
+	}
+	return id
+}
+
+func timeFromRFC3339(val string) time.Time {
+	tm, err := time.Parse(time.RFC3339, val)
+	if err != nil {
+		panic(fmt.Sprintf("TimeFromRFC3339: %v", err))
+	}
+	return tm
+}
+
+func ImportDB(ctx context.Context, c *mongo.Client) {
+	repo := NewMongoRepository(c)
+	if err := repo.ImportDB(ctx, map[string][]bson.M{
+		clients: {
+			{
+				"_id":  objectIDFromHex("client_id_1"),
+				"name": "Mike",
+				"class_ids": []bson.ObjectID{
+					objectIDFromHex("class_id_1"),
+					objectIDFromHex("class_id_2"),
+				},
+			},
+			{
+				"_id":  objectIDFromHex("client_id_2"),
+				"name": "Mike",
+				"class_ids": []bson.ObjectID{
+					objectIDFromHex("class_id_1"),
+					objectIDFromHex("class_id_2"),
+				},
+			},
+		},
+		classes: {
+			{
+				"_id":        objectIDFromHex("class_id_1"),
+				"time":       timeFromRFC3339("2024-10-10T13:00:00Z"),
+				"created_at": time.Now(),
+			},
+		},
+	}); err != nil {
+		panic(fmt.Sprintf("Failed to import DB: %v", err))
+	}
 }
