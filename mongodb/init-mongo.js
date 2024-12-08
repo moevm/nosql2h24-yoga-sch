03b6c4aaee33f11eca4405f57f31e592f306ee97
feyhoa <egorbutylo@mail.ru>
2024-12-08 04:38:29 +0300

    feat(mongodb): add samples

--- mongodb/init-mongo.js
+++ mongodb/init-mongo.js
@@ -1,9 +1,30 @@
+
+const { ObjectId } = require('bson');
+
+
 db = new Mongo().getDB("fitness_aggregator");
 
 db.createCollection('studios', { capped: false });
 db.createCollection('clients', { capped: false });
 db.createCollection('trainers', { capped: false });
 db.createCollection('classes', { capped: false });
+
+
+// TRAINERS
+
+db.trainers.insertOne(
+    {
+        "name": "Boris Va",
+        "phone": "+7(999)444-4444",
+        "gender": "MALE",
+        "birth_date": new Date("2001-10-28T23:58:18Z"),
+        "created_at": new Date("2024-10-28T23:58:18Z"),
+        "updated_at": new Date("2024-10-29T23:58:18Z"),
+        "picture_uri": "cdn.example.com",
+        "class_ids": [],
+        "studio_id": ObjectId("507f1f77bcf86cd799439011")
+    }
+);
 
 db.clients.insertOne(
     {
