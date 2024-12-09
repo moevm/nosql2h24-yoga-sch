4f2415738df5d7f62a5a5c893ef09a234a801ede
elizaveta-andreeva <elizavetaa40@gmail.com>
2024-12-09 22:08:49 +0300

    chore: add pages titles

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,35 +1,55 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
 import DataView from '@/views/DataView.vue';
 import EntityList from "@/components/EntityList.vue";
 import Auth from "@/components/Auth.vue";
 import AdminPage from "@/components/AdminPage.vue";
 
-const routes = [
+const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         name: 'Auth',
         component: Auth,
+        meta: { title: 'Login - Youga Places' },
     },
     {
         path: '/admin',
         name: 'AdminPage',
         component: AdminPage,
+        meta: { title: 'Admin Dashboard - Youga Places' },
     },
     {
         path: '/admin/data',
         name: 'DataView',
         component: DataView,
+        meta: { title: 'Data View - Admin - Youga Places' },
     },
-    {   path: '/admin/data/:entityType',
+    {
+        path: '/admin/data/:entityType',
         name: 'EntityList',
         component: EntityList,
         props: true,
+        meta: {
+            title: (route: any) => `${route.params.entityType} List - Admin - Youga Places`,
+        },
     },
 ];
+
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    const defaultTitle = 'Youga Places';
+    const title =
+        typeof to.meta.title === 'function'
+            ? to.meta.title(to)
+            : to.meta.title;
+
+    document.title = title || defaultTitle;
+    next();
 });
 
 export default router;
